c6d1efe1fcb72855704472eddeee0214de7925bb
Mel <meljohnpualon@gmail.com>
2022-01-29 22:50:34 +0800

    added results component and function

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <h1>Reaction Timer</h1>
   <button @click="start" class="btn" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
-      // console.log(this.delay)
+      this.showResults = false
+    },
+    endGame(reactTime) {
+      this.score = reactTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
---
+++ src/components/Results.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Reaction Time: {{ score }}ms</h2>
+        <h3 class="rank">{{ rank }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [ 'score' ],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250) {
+            this.rank = 'Star reflexes 😍'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid reflexes 😎'
+        } else {
+            this.rank = 'Needs more pracice 😁'
+        }
+    }
+}
+</script>
